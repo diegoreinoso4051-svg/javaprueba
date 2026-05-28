35b427290f564075cb205f5206b212358cbbbe96
Diego Reinoso <diegoreinoso@MacBook-Air-de-Diego.local>
2026-05-28 13:31:08 -0500

    hola mundo

--- app/src/main/java/com/example/practica2805/App.java
+++ app/src/main/java/com/example/practica2805/App.java
@@ -4,11 +4,15 @@
 package com.example.practica2805;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        hola_mundo();
     }
+    public static void hola_mundo(){
+        System.out.println("hola mundo");
+    }
+    
+    
+    
 }
